a6a0b5ce66bf5817763cd70547dc5d8a38030187
valseLee <sollleky72@gmail.com>
2022-10-13 09:34:44 +0900

    221013: xcode 시연 init

---
+++ HelloMacOs/HelloMacOs/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HelloMacOS
+//
+//  Created by 이승준 on 2022/10/13.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ HelloMacOs/HelloMacOs/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  HelloMacOS
+//
+//  Created by 이승준 on 2022/10/13.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
