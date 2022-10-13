0f04ad2fee4c7da35082da61f8944f7bb6b0ec41
valseLee <sollleky72@gmail.com>
2022-10-13 09:31:00 +0900

    221013: xcode 시연 init

---
+++ HelloMacOs/HelloMacOs/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HelloMacOs
+//
+//  Created by 이승준 on 2022/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloMacOs/HelloMacOs/HelloMacOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMacOsApp.swift
+//  HelloMacOs
+//
+//  Created by 이승준 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct HelloMacOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
