6cd23d0bc9d50645a04e3a300d8e4949f931c2fc
valseLee <sollleky72@gmail.com>
2022-10-20 22:12:19 +0900

    221020: Typography에 대한 이해

---
+++ Implements/SwiftUI/NavigationTutorial/NavigationTutorial/NavigationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTutorialApp.swift
+//  NavigationTutorial
+//
+//  Created by 이승준 on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Implements/SwiftUI/PlayWithStacks/PlayWithStacks/UpDownView.swift
@@ -0,0 +1,20 @@
+//
+//  UpDownView.swift
+//  PlayWithStacks
+//
+//  Created by 이승준 on 2022/10/20.
+//
+
+import SwiftUI
+
+struct UpDownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpDownView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpDownView()
+    }
+}
