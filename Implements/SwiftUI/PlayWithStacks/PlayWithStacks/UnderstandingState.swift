b4ebcfc5883dce7886e3da99cae91ffaec29cbe4
valseLee <sollleky72@gmail.com>
2022-10-21 22:50:01 +0900

    221021: 오전 특강, 앱 개발자가 알아야할 기본 상식들

---
+++ Implements/SwiftUI/PlayWithStacks/PlayWithStacks/UnderstandingState.swift
@@ -0,0 +1,20 @@
+//
+//  UnderstandingState.swift
+//  PlayWithStacks
+//
+//  Created by 이승준 on 2022/10/21.
+//
+
+import SwiftUI
+
+struct UnderstandingState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UnderstandingState_Previews: PreviewProvider {
+    static var previews: some View {
+        UnderstandingState()
+    }
+}
