b095ab658282d43ac3f3b3a76ca11e05ad796e5c
valseLee <sollleky72@gmail.com>
2022-10-22 01:48:02 +0900

    add: 시작뷰와 룰렛뷰 추가

---
+++ Implements/SwiftUI/PlayWithStacks/PlayWithStacks/MenuSelectStartView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuSelectStartView.swift
+//  PlayWithStacks
+//
+//  Created by 이승준 on 2022/10/22.
+//
+
+import SwiftUI
+
+struct MenuSelectStartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuSelectStartView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuSelectStartView()
+    }
+}
