ef410795b0732f1d1a5107dec3fc6060cdae71a1
valseLee <sollleky72@gmail.com>
2022-10-24 23:12:18 +0900

    feat: 내부 로직을 책임질 모델 생성, ObservedObject 활용

--- Implements/SwiftUI/UnderstandingEnvironment/UnderstandingEnvironment/DoubleRoulette/Model/MenuData.swift
+++ Implements/SwiftUI/UnderstandingEnvironment/UnderstandingEnvironment/DoubleRoulette/Model/MenuData.swift
@@ -8,23 +8,55 @@
 import Foundation
 import Combine
 
-final class RouletteData: ObservableObject {
-	@Published var menuResultList: [String] = [""]
-	
+final class MenuData: ObservableObject {
 	@Published var userNumber: Int = 10
 	@Published var rotation: Int = 0
 	@Published var titleTextOpacity: CGFloat = 0.0
 	@Published var resultTextOpacity: CGFloat = 0.0
 	
-	func resetData() {
+	public var isPlayed: Bool = false
+	
+	public let menuDictionary: [String: [String]] = [
+		"Lunch" :
+			["부대찌개", "돈까스", "불백", "김치찜", "마카롱", "육개장", "짜장면", "초밥", "스파게티", "삼겹살"],
+		"Dinner" :
+			["해물탕", "라멘", "탕수육", "파스타", "피자", "김피탕", "돼지국밥", "선지국", "감자탕", "초콜렛"]
+	]
+	
+	public var menuResultList: [String] = ["아직 메뉴를 고르지 않았군요!"]
+	
+	public func resetData() {
 		userNumber = 10
 		rotation = 0
 		titleTextOpacity = 0.0
 		resultTextOpacity = 0.0
+		isPlayed = false
 	}
 	
-	func menuResultListUpdate(menuName: String) {
-		self.menuResultList.append(menuName)
+	public func menuResultListUpdate(menuName: String, mealTime: String) {
+		let result = "\(menuResultList.count)회차 " + "\(mealTime) 메뉴 : " + menuName
+		self.menuResultList.append(result)
+		print(menuResultList)
+	}
+	
+	public func getCurrentIndex(rotation: Int, userNumber: Int) -> Int {
+		var offsetRotation: Int = 270 - rotation % 360
+		var index: Int = 0
+		
+		if offsetRotation < 0 {
+			offsetRotation = 360 + offsetRotation
+		}
+		
+		while offsetRotation > 0 {
+			index += 1
+			offsetRotation -= Int(360 / userNumber)
+		}
+		
+		return (index - 1)
+	}
+	
+	public func toggleIsPlayed() {
+		isPlayed = true
 	}
 }
 
