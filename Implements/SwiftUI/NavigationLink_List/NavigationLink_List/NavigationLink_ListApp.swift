37e0f73f5b1afd11598f2554c3e7714f0c9f889b
valseLee <sollleky72@gmail.com>
2022-10-25 10:01:51 +0900

    init: init

---
+++ Implements/SwiftUI/NavigationLink_List/NavigationLink_List/NavigationLink_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLink_ListApp.swift
+//  NavigationLink_List
+//
+//  Created by 이승준 on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLink_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
