a0f8e1f1bd9bf726703d2690be72e12ff6ab2c94
valseLee <sollleky72@gmail.com>
2022-11-01 23:51:30 +0900

    init: 타이머 예시코드

---
+++ Implements/SwiftUI/UnderstandingGesture/UnderstandingGesture/TimerView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerView.swift
+//  UnderstandingGesture
+//
+//  Created by 이승준 on 2022/11/01.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
