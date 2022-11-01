8dae71521af8bfef5202f76bbc51670e2483d21d
valseLee <sollleky72@gmail.com>
2022-11-01 23:52:20 +0900

    221101: 제스쳐 내용 진행

--- Implements/SwiftUI/UnderstandingGesture/UnderstandingGesture/ContentView.swift
+++ Implements/SwiftUI/UnderstandingGesture/UnderstandingGesture/ContentView.swift
@@ -8,19 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var moveValue = 0
+	@State private var isMoving = false
+	
+	@State private var isTimerRunning = false
+	@State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+	@GestureState private var dragEnabled = false
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+		
+		return VStack {
+			ZStack {
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+				Circle()
+					.fill(.clear)
+					.overlay {
+						Circle().stroke(Color.black, lineWidth: 10)
+					}
+					.frame(width: 250, height: 250)
+				
+				Image(systemName: "star.fill")
+					.resizable()
+					.foregroundColor(.blue)
+					.offset(y: -125)
+					.frame(width: 25, height: 25)
+					.onReceive(timer) { _ in
+						if self.isTimerRunning {
+							moveValue += 10
+						}
+					}
+					.rotationEffect(.degrees(Double(moveValue)))
+					.animation(.easeInOut, value: moveValue)
+					.animation(.spring(response: 0.55, dampingFraction: 0.825, blendDuration: 0), value: moveValue)
+			}
+			
+			
+			Button(action: {
+			}) {
+				VStack {
+					Text("Move it")
+						.gesture(TapGesture().onEnded({ _ in
+							moveValue += 100
+							isMoving = true
+						}))
+						.gesture(LongPressGesture(minimumDuration: 1.5)
+								.onEnded({ isLongTappedEnd in
+									isTimerRunning.toggle()
+									self.startTimer()
+								}))
+						
+				}
+			}.padding(.vertical, 15)
+			
+			if isTimerRunning {
+				Button("Reset") {
+					moveValue = 0
+					isMoving = false
+					self.stopTimer()
+					isTimerRunning = false
+				}
+			}
+			
+			if isMoving {
+				Text("To Left or Right")
+					.gesture(LongPressGesture(minimumDuration: 1.0).updating($dragEnabled, body: { value, state, trans in
+						state = value
+						print("??")
+					}).sequenced(before: DragGesture().onChanged({ value in
+						moveValue = Int(value.location.x)
+					})))
+			}
+		}
+		.onAppear {
+			self.stopTimer()
+		}
     }
+	
+	private func startTimer() {
+		self.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+	}
+	
+	private func stopTimer() {
+		self.timer.upstream.connect().cancel()
+	}
 }
--- Implements/SwiftUI/UnderstandingGesture/UnderstandingGesture/TimerView.swift
+++ Implements/SwiftUI/UnderstandingGesture/UnderstandingGesture/TimerView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct TimerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@State var isTimerRunning = false
+	@State private var startTime =  Date()
+	@State private var timerString = "0.00"
+	@State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+	
+	var body: some View {
+		
+		Text(self.timerString)
+			.font(Font.system(.largeTitle, design: .monospaced))
+			.onReceive(timer) { _ in
+				if self.isTimerRunning {
+					timerString = String(format: "%.2f", (Date().timeIntervalSince( self.startTime)))
+				}
+			}
+			.onTapGesture {
+				if isTimerRunning {
+					// stop UI updates
+					self.stopTimer()
+				} else {
+					timerString = "0.00"
+					startTime = Date()
+					// start UI updates
+					self.startTimer()
+				}
+				isTimerRunning.toggle()
+			}
+			.onAppear() {
+				// no need for UI updates at startup
+				self.stopTimer()
+			}
+	}
+	
+	func stopTimer() {
+		self.timer.upstream.connect().cancel()
+	}
+	
+	func startTimer() {
+		self.timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+	}
 }
 
 struct TimerView_Previews: PreviewProvider {
