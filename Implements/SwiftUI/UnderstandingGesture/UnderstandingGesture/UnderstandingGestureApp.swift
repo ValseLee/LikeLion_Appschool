d81238e7b6ff5f4d3b250f11a93f100ad7ce1b4a
valseLee <sollleky72@gmail.com>
2022-11-01 11:06:50 +0900

    init: Gesture Practice Init

---
+++ Implements/SwiftUI/UnderstandingGesture/UnderstandingGesture/UnderstandingGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingGestureApp.swift
+//  UnderstandingGesture
+//
+//  Created by 이승준 on 2022/11/01.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
