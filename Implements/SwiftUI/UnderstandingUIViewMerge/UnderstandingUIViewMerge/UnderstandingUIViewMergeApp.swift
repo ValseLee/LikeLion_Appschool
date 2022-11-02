7f8f72063439d378e1ba441c505a1f03ae03b3a7
valseLee <sollleky72@gmail.com>
2022-11-02 10:18:34 +0900

    init: init

---
+++ Implements/SwiftUI/UnderstandingUIViewMerge/UnderstandingUIViewMerge/UnderstandingUIViewMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingUIViewMergeApp.swift
+//  UnderstandingUIViewMerge
+//
+//  Created by 이승준 on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingUIViewMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
