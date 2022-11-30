5aad485c0228a4abc54d8a28a9e3cb6e7a7d9e69
valseLee <sollleky72@gmail.com>
2022-11-30 20:24:22 +0900

    ~221130: Multiplatform 테스트

---
+++ Implements/SwiftUI/MultiplatformTest/MultiplatformTest/MenuItems.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItems.swift
+//  MultiplatformTest
+//
+//  Created by 이승준 on 2022/11/30.
+//
+
+import Foundation
---
+++ Implements/SwiftUI/MultiplatformTest/MultiplatformTest/MultiplatformTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformTestApp.swift
+//  MultiplatformTest
+//
+//  Created by 이승준 on 2022/11/30.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
