bf6cbdd35ca8e5ef53532521a075f69b09bc8248
valseLee <sollleky72@gmail.com>
2022-12-06 21:27:55 +0900

    221206: 파이어베이스를 활용한 realtime DB 구현실습

---
+++ Implements/FireBaseTeset/FireBaseTeset/FireBaseTesetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseTesetApp.swift
+//  FireBaseTeset
+//
+//  Created by 이승준 on 2022/12/06.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseTesetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
